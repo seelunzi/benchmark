2d5a2d84d65dc94c12eade34ab932695d66f0834
Shinelon <418304914@qq.com>
2018-02-22 10:54:46 +0800

    集成netty demo工程

---
+++ nettyexample/src/main/java/com/tangtang/nettyexample/NettyexampleApplication.java
@@ -0,0 +1,12 @@
+package com.tangtang.nettyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyexampleApplication.class, args);
+    }
+}
